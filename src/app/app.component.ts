e2e4b19adf5fccdbdc7268f2dd0bd28dc2b3a17a
philippialex <bloodsoky5@gmail.com>
2023-02-22 23:38:12 +0100

    webtech uloha 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+
+
+export enum Menu {
+  BOOKS = 'BOOKS',
+  USERS = 'USERS',
+  GENRE = 'GENRE',
+  BORROWINGS = 'BORROWINGS'
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,74 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cvicenie1';
+  form: FormGroup;
+  menu = Menu;
+  actualMenu: Menu = Menu.USERS;
+  changeMenu(menuItem: Menu): void {
+    this.actualMenu = menuItem;
+  }
+
+  persons: Array<{
+    name: string;
+    surname: string;
+    contact: string;
+  }> = [];
+
+  books: Array<{
+    name: string;
+    id: string;
+    author: string;
+    available: string;
+  }> = [];
+
+  genre: Array<{
+    name: string;
+    genre: string;
+  }> = [];
+
+  borrowings: Array<{
+    id: string;
+    book: string;
+    user: string;
+  }> = [];
+
+  constructor() {
+    this.form = new FormGroup({
+      name: new FormControl(),
+      surname: new FormControl(),
+      contact: new FormControl(),
+      id: new FormControl(),
+      author: new FormControl(),
+      available: new FormControl(),
+      book: new FormControl(),
+      user: new FormControl(),
+      genre: new FormControl()
+    })
+
+  }
+
+
+  savePerson(): void {
+    console.log('OSOBA:', this.form.value);
+    this.persons.push(this.form.value);
+    this.form.reset();
+  }
+
+  saveBook(): void {
+    console.log('KNIHA:', this.form.value);
+    this.books.push(this.form.value);
+    this.form.reset();
+  }
+
+  saveGenre(): void {
+    console.log('ŽÁNER:', this.form.value);
+    this.genre.push(this.form.value);
+    this.form.reset();
+  }
+
+  saveBorrowing(): void {
+    console.log('VÝPOZICKA:', this.form.value);
+    this.borrowings.push(this.form.value);
+    this.form.reset();
+  }
 }
